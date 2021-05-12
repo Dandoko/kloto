30ea8cd1da4be9ffc49d9cb754c0e098c39afb89
Daniel Ko <kodaniel2015@gmail.com>
2021-05-12 01:58:50 -0400

    ver0008 player script

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    private bool spacebarPressed;
+    private float horizontalInput;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -16,7 +19,22 @@ public class Player : MonoBehaviour
         // Check if space key is pressed down
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            
+            spacebarPressed = true;
         }
+
+        // All horizontal axis inputs
+        horizontalInput = Input.GetAxis("Horizontal");
+    }
+
+    // Called once every physics update (deault is 100/second)
+    private void FixedUpdate()
+    {
+        if (spacebarPressed)
+        {
+            GetComponent<Rigidbody>().AddForce(Vector3.up * 5, ForceMode.VelocityChange);
+            spacebarPressed = false;
+        }
+
+        GetComponent<Rigidbody>().velocity = new Vector3(horizontalInput, GetComponent<Rigidbody>().velocity.y, 0);
     }
 }
