e85f578087aa81f4bb933746bd338cf175768c0a
calumclark15 <67848490+calumclark15@users.noreply.github.com>
2021-05-26 15:24:13 -0700

    Added near clipping plane, removed optical glitches

--- Assets/Scripts/CC Scripts/PortalCameraMovement.cs
+++ Assets/Scripts/CC Scripts/PortalCameraMovement.cs
@@ -16,28 +16,30 @@ public class PortalCameraMovement : MonoBehaviour
     private Matrix4x4 rotMatrix; 
 
     private Vector4 nearClipPlane;
+    private Vector4 nearClipPlaneWorld;
     private Vector3[] portalCoords = new Vector3[3];
     private Plane portalScreenPlane;
     private Camera camComponent;
+    int sign;
 
 
     // Start is called before the first frame update
     void Start()
     {
+        camComponent = GetComponent<Camera>();
+
         portalCoords[0] = lookingAtPortal.transform.GetChild(0).gameObject.GetComponent<Renderer>().bounds.max;
         portalCoords[1] = lookingAtPortal.transform.GetChild(0).gameObject.GetComponent<Renderer>().bounds.min;
         portalCoords[2] = lookingAtPortal.transform.GetChild(0).gameObject.GetComponent<Renderer>().bounds.center;
-        portalScreenPlane = new Plane(portalCoords[0], portalCoords[1], portalCoords[2]);
+        portalScreenPlane = new Plane(-Vector3.Cross(portalCoords[0], portalCoords[1]), portalCoords[2]);
+        //portalScreenPlane = new Plane(-lookingAtPortal.transform.forward, lookingAtPortal.transform.position);
 
-        camComponent = GetComponent<Camera>();
-
-        DrawPlane(portalCoords[2], Vector3.Cross(portalCoords[0], portalCoords[1]));
+        DrawPlane(portalCoords[2], portalScreenPlane.normal);
     }
 
     // Update is called once per frame
     void Update()
     {
-
         //Mimics the player camera rotations relative to portal 1
         rotPlayerToPortal1 = Quaternion.Inverse(renderingOnPortal.transform.rotation) * playerCamera.transform.rotation;
         rotCameraToPortal2 = rotPlayerToPortal1;
@@ -50,21 +52,24 @@ public class PortalCameraMovement : MonoBehaviour
         //Calculates a transformation matrix that would rotate any vector by the difference in rotation of the two portals
         rotMatrix = Matrix4x4.Rotate(portalRotDif);
 
-        //Positions the camera the same distance from portal 2 as the player is from portal 1 VVV THIS IS CORRECT
+        //Positions the camera the same distance from portal 2 as the player is from portal 1
         playerToPortal1 = playerCamera.transform.position - renderingOnPortal.transform.position;
         cameraToPortal2 = rotMatrix.MultiplyPoint(playerToPortal1);
         transform.position = cameraToPortal2 + lookingAtPortal.transform.position;
 
 
-        //nearClipPlane = new Vector4(portalScreenPlane.normal.x, portalScreenPlane.normal.y, portalScreenPlane.normal.z, -Vector3.Dot(portalScreenPlane.normal, transform.position));
-        nearClipPlane = new Vector4(portalScreenPlane.normal.x, portalScreenPlane.normal.y, portalScreenPlane.normal.z, -Vector3.Dot(portalScreenPlane.normal, portalCoords[0]));
+        //Changes the normal vector of the plane so that the near clip plane is always facing the right direction, lowering the possibility of optical glitches
+        sign = System.Math.Sign(Vector3.Dot(renderingOnPortal.transform.forward, playerCamera.transform.position - renderingOnPortal.transform.position));
 
+        //Creates a near clip plane and transforms it to world coordinates
+        nearClipPlane = new Vector4(portalScreenPlane.normal.x * sign, portalScreenPlane.normal.y * sign, portalScreenPlane.normal.z * sign, -portalScreenPlane.distance);
+        nearClipPlaneWorld = Matrix4x4.Transpose(Matrix4x4.Inverse(camComponent.worldToCameraMatrix)) * nearClipPlane;
 
-        camComponent.projectionMatrix = camComponent.CalculateObliqueMatrix(nearClipPlane);
+        camComponent.projectionMatrix = camComponent.CalculateObliqueMatrix(nearClipPlaneWorld);
     }
 
 
-    //FOR DEBUGGING
+    //FOR DEBUGGING vv
     void DrawPlane(Vector3 position,Vector3 normal )
     {
 
