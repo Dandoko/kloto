18f860820e0dff8b7dd4c78c5ed7b09bfb646ec2
Daniel Ko <kodaniel2015@gmail.com>
2021-06-23 21:29:21 -0400

    continuous collision detection

--- Assets/Scripts/Daniel/BulletManager.cs
+++ Assets/Scripts/Daniel/BulletManager.cs
@@ -4,21 +4,39 @@ using UnityEngine;
 
 public class BulletManager
 {
-    private GameObject bulletGameObject;
-    private float speed = 30f;
+    //=========================================================================
+    // General
+    //=========================================================================
     private GunManager gunManager;
     private PortalManager portalManager;
-    private RaycastHit hitObject;
-    private Material bulletMat;
     private Transform playerCamera;
+
+    //=========================================================================
+    // Bullet
+    //=========================================================================
+    private GameObject bulletGameObject;
+    private const float speed = 40f;
+    private RaycastHit bulletDest;
+    private Material bulletMat;
+    private LayerMask bulletMask = LayerMask.NameToLayer("Bullet");
+
+    //=========================================================================
+    // Continuous Collision Detection
+    //=========================================================================
+    private const float skinWidth = 0.1f;  
+    private float minimumExtent;
+    private float partialExtent;
+    private float sqrMinimumExtent;
+    private Vector3 previousPosition;
+    private Collider bulletCollider;
 
     public BulletManager(
         GunManager gunManager, PortalManager portalManager, GameObject bullet, Material bulletMat,
-        Transform gunTip, RaycastHit hitObject, Transform playerCamera)
+        Transform gunTip, RaycastHit bulletDest, Transform playerCamera)
     {
         this.gunManager = gunManager;
         this.portalManager = portalManager;
-        this.hitObject = hitObject;
+        this.bulletDest = bulletDest;
         this.bulletMat = bulletMat;
         this.playerCamera = playerCamera;
 
@@ -26,9 +44,15 @@ public class BulletManager
         bulletGameObject = bullet;
         bulletGameObject.GetComponent<MeshRenderer>().material = bulletMat;
         bulletGameObject.transform.position = gunTip.position;
-        Vector3 bulletDir = (hitObject.point - gunTip.position).normalized;
+        Vector3 bulletDir = (bulletDest.point - gunTip.position).normalized;
         bulletGameObject.transform.forward = bulletDir;
         bulletGameObject.AddComponent<Bullet>();
+
+        bulletCollider = bulletGameObject.GetComponent<Collider>();
+        previousPosition = bulletGameObject.transform.position;
+        minimumExtent = Mathf.Min(Mathf.Min(bulletCollider.bounds.extents.x, bulletCollider.bounds.extents.y), bulletCollider.bounds.extents.z);
+        partialExtent = minimumExtent * (1.0f - skinWidth);
+        sqrMinimumExtent = minimumExtent * minimumExtent;
     }
 
     public void updateBullet()
@@ -36,11 +60,37 @@ public class BulletManager
         if (null != bulletGameObject)
         {
             bulletGameObject.transform.position += bulletGameObject.transform.forward * speed * Time.deltaTime;
+            continuousCollisionDetection();
         }
         else
         {
             gunManager.removeBullet(this);
-            portalManager.createPortal(hitObject, playerCamera, bulletMat);
+            portalManager.createPortal(bulletDest, playerCamera, bulletMat);
         }
+    }
+
+    private void continuousCollisionDetection()
+    {
+        Vector3 movementThisFrame = bulletGameObject.transform.position - previousPosition;
+        float movementSqrMagnitude = movementThisFrame.sqrMagnitude;
+
+        // Checking if the bullet moved more than the minimum extent
+        if (movementSqrMagnitude > sqrMinimumExtent)
+        {
+            float movementMagnitude = Mathf.Sqrt(movementSqrMagnitude);
+            RaycastHit hitObject;
+
+            // Check for objects that were missed 
+            if (Physics.Raycast(previousPosition, movementThisFrame, out hitObject, movementMagnitude, bulletMask))
+            {
+                // If the raycast hit something, and the collided object is not an isTrigger object
+                if (!hitObject.collider.isTrigger)
+                {
+                    // Move the bullet to where the collision occurred
+                    bulletGameObject.transform.position = hitObject.point - (movementThisFrame / movementMagnitude) * partialExtent;
+                }
+            }
+        }
+        previousPosition = bulletGameObject.transform.position;
     }
 }
