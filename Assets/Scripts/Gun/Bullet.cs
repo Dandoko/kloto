4bb4543deb5d5df7cbd74dcdedf7b92329aa4076
Daniel Ko <kodaniel2015@gmail.com>
2021-08-24 17:28:19 -0400

    destroying bullet fix

--- Assets/Scripts/Gun/Bullet.cs
+++ Assets/Scripts/Gun/Bullet.cs
@@ -4,13 +4,14 @@ using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
+    // The bullet must travel fast enough to hit any surfaces before this time limit runs out
     private const int dyingTime = 3;
 
     // Start is called before the first frame update
     void Start()
     {
         // Destroys the bullet using a timer
-        Destroy(gameObject, dyingTime);
+        Destroy(transform.parent.gameObject, dyingTime);
     }
 
     void OnCollisionEnter(Collision collision)
